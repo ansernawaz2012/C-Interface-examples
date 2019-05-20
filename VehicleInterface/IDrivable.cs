79ed2c19df5cabdfb1c912027c94748792621519
ansernawaz2012 <ansernawaz2012@gmail.com>
2019-05-20 11:39:55 +0100

    Add project files.

---
+++ VehicleInterface/IDrivable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInterface
+{
+    interface IDrivable
+    {
+        int Wheels
+        { get; set; }
+
+        double Speed { get; set; }
+
+        void Move();
+        void Stop();
+
+
+    }
+}
---
+++ VehicleInterface/Television.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInterface
+{
+    class Television : IElectronicDevice
+    {
+
+        public int Volume { get; set; }
+
+        public void Off()
+        {
+            Console.WriteLine("The TV is Off");
+        }
+
+        public void On()
+        {
+            Console.WriteLine("The TV is On");
+        }
+
+        public void VolumeDown()
+        {
+            if (Volume > 0)
+            {
+                Volume--;
+                Console.WriteLine($"The TV Volume is at {Volume}");
+
+            }
+        }
+
+        public void VolumeUp()
+        {
+            if (Volume < 99)
+            {
+                Volume++;
+                Console.WriteLine($"The TV Volume is at {Volume}");
+            }
+        }
+    }
+}
